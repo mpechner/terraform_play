3276b802e00115cd669a002c7d66400b6a637c3a
Michael Pechner <mikey@mikey.com>
2022-08-03 20:34:56 -0700

    Create outputs for public and private subnet groups

---
+++ vpc/output.tf
@@ -0,0 +1,13 @@
+output "private_subnets" {
+  value = tolist(aws_network_acl.private.subnet_ids)
+}
+output "public_subnets" {
+  value = tolist(aws_network_acl.public.subnet_ids)
+}
+output "eks_subnets" {
+  value = tolist(aws_network_acl.eks.subnet_ids)
+}
+
+output "all_private" {
+  value = concat ( tolist(aws_network_acl.eks.subnet_ids), tolist(aws_network_acl.private.subnet_ids) )
+}
