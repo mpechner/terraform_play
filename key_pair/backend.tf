68547e829c7b053bb43341997cd16c795de097f5
Michael Pechner <mikey@mikey.com>
2022-09-03 11:17:53 -0700

    updated provider

--- key_pair/backend.tf
+++ key_pair/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mikey-com-terraformstate"
-    key    = "key_pair"
+    key    = "us-east-1/key_pair"
     region = "us-east-1"
     dynamodb_table = "terraform-state"
   }
--- key_pair/provider.tf
+++ key_pair/provider.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1"
+}
+
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 3.0"
+  region = "us-east-1"
+  profile = var.profile
 }
